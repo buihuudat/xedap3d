f26799c418b638cf7543f596beeadf7345d0e6a9
Bùi Hữu Đạt <dat54261001@gmail.com>
2022-04-08 16:03:41 +0700

    push all my code to github

--- jsuser/keyframe-material_1.js
+++ jsuser/keyframe-material_1.js
@@ -16,7 +16,7 @@ MaterialApp.prototype.init = function(param)
 	var ambientLight = new THREE.AmbientLight( 0x444444 );
 	this.scene.add(ambientLight);
 
-	var directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+	var directionalLight = new THREE.DirectionalLight(0x00A7D8, 1);
 	directionalLight.position.set(-.3333, 1, .777).normalize();
 	this.scene.add( directionalLight );
 	this.directionalLight = directionalLight;
@@ -32,7 +32,7 @@ MaterialApp.prototype.init = function(param)
 
 MaterialApp.prototype.createGrid = function()
 {
-	var line_material = new THREE.LineBasicMaterial( { color: 0xaaaaaa, opacity: 0.8 } ),
+	var line_material = new THREE.LineBasicMaterial( { color: 0xC38E63, opacity: 0.8 } ),
 		geometry = new THREE.Geometry(),
 		floor = -2, step = 1, size = 66;
 	
@@ -56,63 +56,5 @@ MaterialApp.prototype.createObjects = function()
 	loader.load( 'models/mountain-bike.dae', (deaModel)=>{	
 		this.root.add(deaModel.scene);
 		// const timer = Date.now() * 0.0001
-		console.log(this);
 	});
 }
-// MaterialApp.prototype.createAnimations = function()
-// {
-// 	this.transparencyAnimator = new Sim.KeyFrameAnimator;
-// 	this.transparencyAnimator.init({ 
-// 		interps:
-// 			[
-// 			    { 
-// 			    	keys:[0, .5, 1], 
-// 			    	values:[
-// 			    	        { opacity : 1},
-// 			    	        { opacity : 0},
-// 			    	        { opacity : 1},
-// 			    	        ],
-// 			    	target:this.ballmaterial
-// 			    }, 
-// 			],
-// 		loop: true,
-// 		duration:MaterialApp.animation_time
-// 	});
-
-// 	this.addObject(this.transparencyAnimator);   
-// }
-
-// MaterialApp.prototype.animate = function(animator, on)
-// {
-// 	if (on)
-// 	{
-// 		animator.start();
-// 	}
-// 	else
-// 	{
-// 		animator.stop();
-// 	}
-// }
-
-// MaterialApp.prototype.setAnimateTransparency = function(val)
-// {
-// 	this.animate(this.transparencyAnimator, val);
-// }
-// MaterialApp.animation_time = 4444;
-// MaterialApp.prototype.animate = function () {
-
-// 	requestAnimationFrame( this.animate );
-
-// 	this.render();
-
-// }
-
-// MaterialApp.prototype.render = function () {
-// 	const timer = Date.now() * 0.0001;
-// 	this.camera.position.x = Math.cos( timer ) * 20;
-// 	this.camera.position.y = 10;
-// 	this.camera.position.z = Math.sin( timer ) * 20;
-// 	this.camera.lookAt(this.root.position);
-// 	this.renderer.render( this.scene, this.camera );
-
-// }
